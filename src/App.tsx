88ad2c0359de500e6988cd9e630c164f69d42c18
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-18 19:44:34 +0330

    Adds material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@mui/material';
 import React from 'react';
 
 export default function App() {
     return (
         <div>
+            <Button variant="contained">Hello World</Button>
         </div>
     );
 };
